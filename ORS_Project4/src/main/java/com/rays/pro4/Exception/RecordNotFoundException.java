cdfeb3fc11fcbde1cf3beea094f708c102a7ffcf
namanmeena05 <meenanaman641@gmail.com>
2023-09-21 13:57:18 +0530

    ok

---
+++ ORS_Project4/src/main/java/com/rays/pro4/Exception/RecordNotFoundException.java
@@ -0,0 +1,12 @@
+package com.rays.pro4.Exception;
+/**
+ * RecordNotFoundException thrown when a record not found occurred.
+ * 
+ * @author Naman Meena
+ * 
+ */
+public class RecordNotFoundException extends Exception {
+	public RecordNotFoundException(String msg){
+		super(msg);
+	}
+}
